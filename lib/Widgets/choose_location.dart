587e9ad8c7880c6c2a83f025ef38c4081efa9b55
Shedrack <shedrackejeh458@gmail.com>
2021-01-15 23:52:50 +0100

    done

--- lib/Widgets/choose_location.dart
+++ lib/Widgets/choose_location.dart
@@ -6,6 +6,27 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  void getData() async {
+    //Simulate a network request for username,
+    String userName = await Future.delayed(Duration(seconds: 3), () {
+      return "My Name Is Shedrack";
+    });
+
+    //Simulate network request to get username bio.
+    String bio = await Future.delayed(Duration(seconds: 2), () {
+      return "Vegan, village, riverpod, provider";
+    });
+
+    print("$userName - $bio");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    print("Siberian Husky");
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +36,7 @@ class _ChooseLocationState extends State<ChooseLocation> {
         elevation: 0.0,
         centerTitle: true,
       ),
-      body: Text("Choose Location Screen"),
+      body: Text("Getx State Management"),
     );
   }
 }

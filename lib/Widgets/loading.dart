0fe7354e49c070c75c97c02bda2a38123337216f
Shedrack <shedrackejeh458@gmail.com>
2021-01-17 00:07:39 +0100

    done

--- lib/Widgets/loading.dart
+++ lib/Widgets/loading.dart
@@ -1,6 +1,5 @@
 import 'package:InstaClock/services/world_time.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -8,15 +7,14 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  String time = "loading";
-
   void setUpWorldTime() async {
     WorldTime instance = WorldTime(
         location: "Berlin", flag: "nigeria.png", url: "Europe/Berlin");
     await instance.getTime();
-    print(instance.time);
-    setState(() {
-      time = instance.time;
+    Navigator.pushReplacementNamed(context, "/homePage", arguments: {
+      "location": instance.location,
+      "flag": instance.flag,
+      "time": instance.time,
     });
   }
 
@@ -29,20 +27,20 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        leading: Icon(FontAwesomeIcons.clock, color: Colors.black),
-        title: Text(
-          "InstaClock",
-          style: TextStyle(color: Colors.black),
-        ),
-        centerTitle: true,
-      ),
+      // appBar: AppBar(
+      //   backgroundColor: Colors.white,
+      //   leading: Icon(FontAwesomeIcons.clock, color: Colors.black),
+      //   title: Text(
+      //     "InstaClock",
+      //     style: TextStyle(color: Colors.black),
+      //   ),
+      //   centerTitle: true,
+      // ),
       body: Center(
         child: Padding(
           padding: EdgeInsets.all(50.0),
           child: Text(
-            "$time",
+            "Loading",
             style: TextStyle(
               fontSize: 18.0,
               fontWeight: FontWeight.w600,
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart';
+import 'package:intl/intl.dart';
 
 class WorldTime {
   String location; //location name for the UI,
@@ -12,24 +13,27 @@ class WorldTime {
 
   Future<void> getTime() async {
     //make the request call
-    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
-    Map data = jsonDecode(response.body);
-    // print(data);
+    try {
+      Response response =
+          await get('http://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body);
+      // print(data);
 
-    //get properties from data
-    String datetime = data["datetime"];
-    String offset = data["utc_offset"].substring(1, 3);
-    // print(datetime);
-    // print(offset);
+      //get properties from data
+      String datetime = data["datetime"];
+      String offset = data["utc_offset"].substring(1, 3);
+      // print(datetime);
+      // print(offset);
 
-    //create datetime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
+      //create datetime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
 
-    //set the time property,
-    time = now.toString();
+      //set the time property,
+      time = DateFormat.jm().format(now);
+    } catch (e) {
+      print("No such method: $e");
+      time = "Could not get the time data...";
+    }
   }
 }
-
-WorldTime instance =
-    WorldTime(location: "Berlin", flag: "nigeria.png", url: "Europe/Berlin");
